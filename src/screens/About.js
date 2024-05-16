780ff36ac5c8d8cf9a91de81c8097cc928791896
Pereira Damian <dpereira3@abc.gob.ar>
2024-05-16 10:56:17 -0300

    Primero

---
+++ src/screens/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className="container">
+            <h3 className="text-center text-uppercase py-4">About us</h3>
+            <div className="text-muted shadow-sm rounded p-4 lh-2">
+                <p>
+                Sed venenatis fermentum orci, sit amet mollis sapien hendrerit nec. Cras elementum tincidunt condimentum. Quisque faucibus eu quam et sagittis. Pellentesque in nunc lorem. In pellentesque nisl nisl, et scelerisque lectus feugiat sit amet. Sed tincidunt id tortor vel aliquam. Pellentesque pharetra lorem vitae sollicitudin mollis. Nullam elementum justo in dolor auctor lacinia. Morbi nec mi aliquet, molestie nisl id, dignissim enim. Morbi ac vulputate nisl. Phasellus tristique at urna nec malesuada. Donec non luctus ante. Quisque pharetra posuere enim eu lacinia. Donec viverra lectus eget faucibus elementum. 
+                </p>
+                <p>
+                Praesent nec orci quis mi commodo eleifend. Cras ex purus, varius eu elit sit amet, aliquet venenatis est. Suspendisse consectetur lacinia sem, a egestas neque accumsan vitae. Vestibulum lacinia dui vel eros suscipit luctus. Phasellus molestie interdum dolor, sed sollicitudin eros vehicula nec. Nam pulvinar felis fermentum porttitor hendrerit. Pellentesque rutrum diam eu lacus elementum aliquet. Proin diam erat, tristique et elit non, aliquam auctor mi. Integer sagittis tellus enim, condimentum lobortis libero ornare sed. Suspendisse pellentesque arcu vitae rhoncus elementum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
